1534a8697b368e1ac0e0088a59a93869f55cf030
Airat Galievich <abulghar@mailru>
2024-04-15 16:11:25 +0500

    react

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Practice from './components/Practice';
+import Img from './components/Img';
+import Practice2 from './components/Practice2';
+import Practice3 from './components/Practice3';
+
+
+function App() {
+  return (
+    <div>
+      <Practice/>
+      <Img/>
+      <Practice2/>
+      <Practice3/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
